73ff5f6274aadfab1fa82cfde571e5c26de503ab
mufidbrown <ilhammufid67@gmail.com>
2023-11-24 16:07:59 +0700

    Add Perulangan

---
+++ src/main/java/controlflow/perulangan/While.java
@@ -0,0 +1,12 @@
+package controlflow.perulangan;
+
+public class While {
+    public static void main(String[] args) {
+        int value = 1;
+        while (value <= 10) {
+            System.out.print("Angka : " + value);
+            value++;
+            System.out.print("\n");
+        }
+    }
+}
