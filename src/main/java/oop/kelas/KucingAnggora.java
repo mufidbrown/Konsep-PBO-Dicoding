a7dbbb0d01c7babc3de344c24936096a0a50fc3e
mufidbrown <ilhammufid67@gmail.com>
2023-11-26 22:05:55 +0700

    Add OOP, class & objek

---
+++ src/main/java/oop/kelas/KucingAnggora.java
@@ -0,0 +1,15 @@
+package oop.kelas;
+
+public class KucingAnggora {
+
+    String namaKucing;
+    int beratBadan;
+
+    //ini adalah contoh parameterized constructor
+    //dengan memberikan nilai awal data member nama_kucing dan berat_badan
+    //dengan parameter yang kita tentukan
+    public KucingAnggora(String nama, int berat) {
+        namaKucing = nama;
+        beratBadan = berat;
+    }
+}
