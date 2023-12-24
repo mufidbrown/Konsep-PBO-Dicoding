b83873ef0bc264bbf379e4ccf64482028b25a45d
mufidbrown <ilhammufid67@gmail.com>
2023-12-24 19:09:14 +0700

    update access modifier static methods

--- src/main/java/oop/accessmodifier/package3/Perhitungan.java
+++ src/main/java/oop/accessmodifier/package3/Perhitungan.java
@@ -1,5 +1,15 @@
 package oop.accessmodifier.package3;
 
 public class Perhitungan {
+
     public static int nilai = 0;
+
+    //ini tambahan
+    protected static int getNilai(){
+        return nilai;
+    }
+
+    Perhitungan() {
+        nilai++;
+    }
 }
