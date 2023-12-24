8cd98f81c4f264d9be624f299950c4c06e054693
mufidbrown <ilhammufid67@gmail.com>
2023-12-24 19:40:28 +0700

    final create inheritance

--- src/main/java/oop/inheritance/Kucing.java
+++ src/main/java/oop/inheritance/Kucing.java
@@ -1,6 +1,26 @@
 package oop.inheritance;
 
 public class Kucing extends Hewan {
+    private String ras;
+    private String habitat;
+    public Kucing(String ras, String habitat) {
+        this.ras = ras;
+        this.habitat = habitat;
+    }
+    @Override
+    public String toString() {
+        return "Kucing ras: " + ras + ", habitat: " + habitat;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Kucing) {
+            Kucing other = (Kucing) obj;
+            return this.ras.equals(other.ras);
+        } else {
+            return false;
+        }
+    }
+
     public Kucing() {
         //super(); // akan tetap memanggil constructor dari parent Class
         System.out.println("construct Kucing");
@@ -9,6 +29,7 @@ public class Kucing extends Hewan {
     public void makan() { // overriding
         System.out.println("Kucing sedang makan..");
     }
+
     public void makan(String food) { // overloading
         System.out.println("Kucing makan " + food);
     }
--- src/main/java/oop/inheritance/Main.java
+++ src/main/java/oop/inheritance/Main.java
@@ -19,5 +19,16 @@ public class Main {
         hewan.makan();
         kucing.makan();
         kucing.makan("daging ikan");
+
+        System.out.println("------------------------");
+
+        Kucing meow = new Kucing("Ocicat", "tropis");
+        Kucing puss = new Kucing("Ocicat", "subtropis");
+        Kucing popo = new Kucing("Anggora", "subtropis");
+        System.out.println(meow.toString());
+        System.out.println(puss.toString());
+        System.out.println(popo.toString());
+        System.out.println("meow equals puss ? " + meow.equals(puss));
+        System.out.println("meow equals popo ? " + meow.equals(popo));
     }
 }
