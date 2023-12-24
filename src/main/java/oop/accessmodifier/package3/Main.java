749133b4080c9b6c2ba558fc54412806570f5286
mufidbrown <ilhammufid67@gmail.com>
2023-12-24 19:03:19 +0700

    Update access modifier

---
+++ src/main/java/oop/accessmodifier/package3/Main.java
@@ -0,0 +1,7 @@
+package oop.accessmodifier.package3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nilainya adalah " + Perhitungan.nilai);
+    }
+}
